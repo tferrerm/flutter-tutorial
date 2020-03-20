f27d0d7335967f38688b9ac70543d37537a261be
tferrerITBA <tferrer@itba.edu.ar>
2020-03-19 22:51:58 -0300

    Styling improvements

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -25,7 +25,7 @@ class MyHomePage extends StatelessWidget {
     Transaction(
       id: 't2',
       title: 'Groceries',
-      amount: 5.99,
+      amount: 15.99,
       date: DateTime.now(),
     ),
   ];
@@ -47,8 +47,38 @@ class MyHomePage extends StatelessWidget {
             ),
             elevation: 5,
           ),
-          Column(children: transactions.map((transaction) {
-            return Card(child: Text(transaction.title),);
+          Column(
+              children: transactions.map((transaction) {
+            return Card(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(15),
+                    child: Text(
+                      transaction.amount.toString(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Text(transaction.title),
+                      Text(transaction.date.toString())
+                    ],
+                  )
+                ],
+              ),
+            );
           }).toList())
         ],
       ),
