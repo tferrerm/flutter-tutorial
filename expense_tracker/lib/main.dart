962310f98ad597c1b0a1d6f7258bd518fce8cd27
tferrerm <tferrer@itba.edu.ar>
2022-01-18 17:27:31 -0300

    Implement landscape mode

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -7,6 +7,17 @@ import './widgets/transaction_list.dart';
 
 void main() => runApp(MyApp());
 
+// To disable portrait mode, comment out main() above and uncomment the following lines:
+// import 'package:flutter/services.dart';
+// void main() {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   SystemChrome.setPreferredOrientations([
+//     DeviceOrientation.portraitUp,
+//     DeviceOrientation.portraitDown,
+//   ]);
+//   runApp(MyApp());
+// }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -58,6 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
     //   date: DateTime.now(),
     // ),
   ];
+  bool _showChart = false;
 
   List<Transaction> get _recentTransactions {
     final sevenDaysBefore = DateTime.now().subtract(Duration(days: 7));
@@ -100,6 +112,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
     final appBar = AppBar(
       title: Text('Personal Expenses'),
       actions: <Widget>[
@@ -111,7 +125,14 @@ class _MyHomePageState extends State<MyHomePage> {
     );
 
     final screenHeight = MediaQuery.of(context).size.height;
-    final remainingScreenSpace = screenHeight - appBar.preferredSize.height - MediaQuery.of(context).padding.top;
+    final remainingScreenSpace = screenHeight -
+        appBar.preferredSize.height -
+        MediaQuery.of(context).padding.top;
+
+    final txListWidget = Container(
+      height: remainingScreenSpace * 0.7,
+      child: TransactionList(_userTransactions, _deleteTransaction),
+    );
 
     return Scaffold(
       appBar: appBar,
@@ -119,14 +140,34 @@ class _MyHomePageState extends State<MyHomePage> {
         // mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Container(
-            height: remainingScreenSpace * 0.3,
-            child: Chart(_recentTransactions),
-          ),
-          Container(
-            height: remainingScreenSpace * 0.7,
-            child: TransactionList(_userTransactions, _deleteTransaction),
-          ),
+          if (isLandscape)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Show Chart'),
+                Switch(
+                  value: _showChart,
+                  onChanged: (val) {
+                    setState(() {
+                      _showChart = val;
+                    });
+                  },
+                )
+              ],
+            ),
+          if (!isLandscape)
+            Container(
+              height: remainingScreenSpace * 0.3,
+              child: Chart(_recentTransactions),
+            ),
+          if (!isLandscape) txListWidget,
+          if (isLandscape)
+            _showChart
+                ? Container(
+                    height: remainingScreenSpace * 0.7,
+                    child: Chart(_recentTransactions),
+                  )
+                : txListWidget,
         ],
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
