58d127910efc0ac7c90d408352f168993c6e83c2
tferrerm <tferrer@itba.edu.ar>
2022-02-06 14:44:00 -0300

    Add GridView with category widgets

--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './categories_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -14,28 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('DeliMeals'),
-      ),
-      body: const Center(
-        child: Text('Hello world!'),
-      ),
+      home: const CategoriesScreen(),
     );
   }
 }
